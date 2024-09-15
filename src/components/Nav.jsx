36834896aae31b6e1b9533ae596bdc2ece7fa2e7
Natasha Png <natashapng.png@gmail.com>
2024-09-15 22:45:33 +1000

    nav menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,6 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import LibraryLogo from '../assets/Library.svg'
 
 const Nav = () => {
+    function openMenu() {
+        document.body.classList += "menu--open";
+    }
+    function closeMenu() {
+        document.body.classList.remove("menu--open");
+    }
     return (
         <nav>
             <div className="nav__container">
@@ -22,7 +28,7 @@ const Nav = () => {
                         </a>
                     </li>
                     <li>
-                        <button className="btn__menu">
+                        <button className="btn__menu" onClick={openMenu}>
                         <FontAwesomeIcon icon="bars"></FontAwesomeIcon>
                         </button>
                     </li>
@@ -34,7 +40,7 @@ const Nav = () => {
                     </li>
                 </ul>
                 <div className="menu__backdrop">
-                    <button className="btn__menu btn__menu--close">
+                    <button className="btn__menu btn__menu--close" onClick={closeMenu}>
                         <FontAwesomeIcon icon="times"/>
                     </button>
                     <ul className="menu__links">
